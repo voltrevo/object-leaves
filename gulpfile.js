860cca7d302a75c656f2402b6089ea54529aaa8a
Andrew Morris <voltrevo@gmail.com>
2015-09-20 15:35:37 +1000

    Add coveralls

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var path = require('path');
 var gulp = require('gulp');
 var eslint = require('gulp-eslint');
 var excludeGitignore = require('gulp-exclude-gitignore');
@@ -7,6 +8,7 @@ var mocha = require('gulp-mocha');
 var istanbul = require('gulp-istanbul');
 var nsp = require('gulp-nsp');
 var plumber = require('gulp-plumber');
+var coveralls = require('gulp-coveralls');
 
 gulp.task('static', function() {
   return gulp.src('**/*.js')
@@ -43,5 +45,14 @@ gulp.task('test', ['pre-test'], function(cb) {
     });
 });
 
+gulp.task('coveralls', ['test'], function() {
+  if (!process.env.CI) {
+    return undefined;
+  }
+
+  return gulp.src(path.join(__dirname, 'coverage/lcov.info'))
+    .pipe(coveralls());
+});
+
 gulp.task('prepublish', ['nsp']);
-gulp.task('default', ['static', 'test']);
+gulp.task('default', ['static', 'test', 'coveralls']);
